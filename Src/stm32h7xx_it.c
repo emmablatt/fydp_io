5ea6e68d37409abd291eab321fa3169768c17de5
Emma Blatt <elblatt@uwaterloo.ca>
2022-03-01 01:20:06 -0500

    line 1558 in discovery_audio.c. going to find which register setting isn't enabled properly

--- Src/stm32h7xx_it.c
+++ Src/stm32h7xx_it.c
@@ -266,6 +266,8 @@ void AUDIO_IN_SAI_PDMx_DMAx_IRQHandler(void)
 {
   /* USER CODE BEGIN BDMA_Channel0_IRQn 0 */
 	BSP_AUDIO_IN_IRQHandler(PDM, AUDIO_IN_DEVICE_DIGITAL_MIC1);
+	BSP_LED_On(LED1);
+
 	//TODO: BDMA_REQUEST_SAI4_A;
   /* USER CODE END BDMA_Channel0_IRQn 0 */
   //HAL_DMA_IRQHandler(&hdma_sai4_a);
