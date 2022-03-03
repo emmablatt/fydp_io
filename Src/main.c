b107fab5fda25097b55bec15658a2a8554cc3966
Emma Blatt <elblatt@uwaterloo.ca>
2022-03-02 19:17:42 -0500

    fixed typo in main

--- Src/main.c
+++ Src/main.c
@@ -89,7 +89,7 @@ int main(void)
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */r
+  /* USER CODE BEGIN Init */
 
   /* USER CODE END Init */
 
